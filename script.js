1b0b2cf5468576e009a279a4b050ec6cfb4a988c
Oscar <bo.walczak@wp.pl>
2022-11-14 19:37:00 +0100

    Pobranie i przypisanie wartości do zmiennych i wyświetlenie ich na stronie

--- script.js
+++ script.js
@@ -21,33 +21,35 @@
 //     .then(console.log(weather))
 
 
-const city = document.querySelector(".city");
-const temp = document.querySelector(".temp");
-const icon = document.querySelector(".icon");
-const description = document.querySelector(".description");
-const humidity = document.querySelector(".humidity");
-const wind = document.querySelector(".wind");
+
 
 
 const weather = {
+    city: "warsaw",
     apiKey: "670c47e06a966cc34fbe6ea72f070c9d",
-    fetchWeather: async function (city) {
-        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+    fetchWeather: async function () {
+        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + this.city + "&units=metric&appid=" + this.apiKey)
         // .then((data) => console.log(data))
         return this.displayWeather(await response.json());
-        // console.log(data)
     },
 
     displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main
-        const { wind } = data.wind
-        console.log(name, description)
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
 
+        document.querySelector(".city").textContent = `Weather in ${name}`;
+        document.querySelector(".temp").textContent = `${Math.floor(temp)}°C`;
+        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
+        document.querySelector(".description").textContent = description;
+        document.querySelector(".humidity").textContent = `Humidity: ${humidity}%`;
+        document.querySelector(".wind").textContent = `Wind speed: ${speed}km/h`;
+
+        console.log(speed)
     }
 
 }
+weather.fetchWeather()
 
-weather.fetchWeather("warsaw")
 
