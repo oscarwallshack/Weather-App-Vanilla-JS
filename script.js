16d437a23d97eaf91608f1a1cc30b7342a81ab98
Oscar <bo.walczak@wp.pl>
2022-11-15 22:21:04 +0100

    skończona aplikacja

--- script.js
+++ script.js
@@ -1,46 +1,45 @@
+(() => {
+    const weather = {
+        city: "Warsaw",
+        apiKey: "Api Key",
+        fetchWeather: async function () {
+            const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + this.city + "&units=metric&appid=" + this.apiKey)
+            try {
+                if (!response.ok)
+                    throw new Error(response.statusText);
+                return this.displayWeather(await response.json());
+            } catch (error) {
+                console.log(error);
+                document.querySelector(".city").textContent = `City "${this.city}" not found.`;
+            }
+        },
 
-const weather = {
-    city: "Warsaw",
-    apiKey: "670c47e06a966cc34fbe6ea72f070c9d",
-    fetchWeather: async function () {
-        const response = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + this.city + "&units=metric&appid=" + this.apiKey)
-        try {
-            if (!response.ok)
-                throw new Error(response.statusText);
-            return this.displayWeather(await response.json());
-        } catch (error) {
-            console.log(error);
-            document.querySelector(".city").textContent = `City "${this.city}" not found.`;
+        displayWeather: function (data) {
+            const { name } = data;
+            const { icon, description } = data.weather[0];
+            const { temp, humidity } = data.main;
+            const { speed } = data.wind;
+
+            document.querySelector(".city").textContent = `Weather in ${name}`;
+            document.querySelector(".temp").textContent = `${Math.floor(temp)}°C`;
+            document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
+            document.querySelector(".description").textContent = description;
+            document.querySelector(".humidity").textContent = `Humidity: ${humidity}%`;
+            document.querySelector(".wind").textContent = `Wind speed: ${speed}km/h`;
         }
-    },
-
-    displayWeather: function (data) {
-        const { name } = data;
-        const { icon, description } = data.weather[0];
-        const { temp, humidity } = data.main;
-        const { speed } = data.wind;
-
-        document.querySelector(".city").textContent = `Weather in ${name}`;
-        document.querySelector(".temp").textContent = `${Math.floor(temp)}°C`;
-        document.querySelector(".icon").src = `https://openweathermap.org/img/wn/${icon}.png`;
-        document.querySelector(".description").textContent = description;
-        document.querySelector(".humidity").textContent = `Humidity: ${humidity}%`;
-        document.querySelector(".wind").textContent = `Wind speed: ${speed}km/h`;
     }
-}
 
+    document.querySelector('#search_city').addEventListener('click', () => {
+        let city = document.querySelector('#city_input').value;
+        let notif = document.querySelector('.notif');
+        if (city && city != '') {
+            notif.textContent = null;
+            weather.city = city;
+            weather.fetchWeather();
+        } else {
+            notif.textContent = 'Enter city!'
+        }
+    })
 
-document.querySelector('#search_city').addEventListener('click', () => {
-    let city = document.querySelector('#city_input').value;
-    let notif = document.querySelector('.notif');
-    if (city && city != '') {
-        notif.textContent = null;
-        weather.city = city;
-        weather.fetchWeather();
-    } else {
-        notif.textContent = 'Enter city!'
-    }
-})
-
-
-weather.fetchWeather()
+    weather.fetchWeather();
+})()
